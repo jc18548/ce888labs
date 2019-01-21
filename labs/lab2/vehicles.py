8e9a153c8945c54d2385424c8512d5991164e7ce
Contreras Gonzalez J J <jc18548@cseelab700.essex.ac.uk>
2019-01-21 12:31:38 +0000

    ok

---
+++ labs/lab2/vehicles.py
@@ -0,0 +1,16 @@
+import matplotlib
+matplotlib.use('Agg')
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+def mad(arr):
+    """ Median Absolute Deviation: a "Robust" version of standard deviation.
+        Indices variabililty of the sample.
+        https://en.wikipedia.org/wiki/Median_absolute_deviation
+        http://stackoverflow.com/questions/8930370/where-can-i-find-mad-mean-absolute-deviation-in-scipy
+    """
+    arr = np.ma.array(arr).compressed() # should be faster to not use masked arrays.
+    med = np.median(arr)
+    return np.median(np.abs(arr - med))
